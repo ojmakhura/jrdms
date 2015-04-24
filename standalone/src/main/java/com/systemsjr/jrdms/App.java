f1efa261ebd557a207a22f1c82899e80cde691c8
Onalenna Junior Makhura <ojmakhura@systemsjr.com>
2015-04-24 12:46:16 +0200

    new commit

---
+++ standalone/src/main/java/com/systemsjr/jrdms/App.java
@@ -0,0 +1,13 @@
+package com.systemsjr.jrdms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
